d371ee322b4cbebcf46861564407f1e5642c99ad
Subhransu sekhar panda <subhransusekharpanda02@gmail.com>
2025-03-29 18:32:58 +0530

    Commit

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,11 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black-100">
+      <div className="p-6 bg-grey rounded-lg shadow-lg">
+        <SignUp />
+      </div>
+    </main>
+  );
+}
